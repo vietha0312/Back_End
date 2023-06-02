27c385520a054585102f39cad6c1a7fa04277802
NTienDuy <105479766+NTienDuy@users.noreply.github.com>
2023-06-03 00:15:34 +0700

    setup enviroment

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import express from "express";
 
-
-
-
-
+import mongoose from "mongoose";
 const app = express();
 
 app.use(express.json());
 
-
-
+mongoose.connect(
+  "mongodb+srv://pvietha301:03122003@angular123.trvmz8z.mongodb.net/"
+);
 
 export const viteNodeApp = app;
