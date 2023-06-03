aeba5793792687e36a2127c9db08c995c51c85d6
minhnvph20397 <105479880+Zatuzick@users.noreply.github.com>
2023-06-03 15:33:07 +0700

    feat: Signup refs: AA-13

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,1 +1,39 @@
-console.log("Ahihi");
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user";
+import dotenv from "dotenv";
+
+dotenv.config();
+import { signupSchema } from "../schemas/auth";
+export const signup = async (req, res) => {
+    try {
+        const { name, email, password, confirmPassword } = req.body;
+        const { error } = signupSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({
+                message: "Email đã tồn tại",
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        user.password = undefined;
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY, { expiresIn: 60 * 60 });
+        return res.status(201).json({
+            message: "Đăng ký thành công",
+            accessToken: token,
+            user,
+        });
+    } catch (error) { }
+};
